f2595f43d976539f273e4bd93ea3a9e12819773d
Arnav Kanodia <akanodia10@gmail.com>
2019-05-29 15:50:30 +0000

    activity 4 part 4

--- cdemo/myproject/student.c
+++ cdemo/myproject/student.c
@@ -1,12 +1,5 @@
+#include "student.h"
 #include <stdio.h>
-#include <string.h>
-
-struct Student {
-  char firstName[50];
-  char lastName[50];
-  int age;
-  int studentID;
-};
 
 void printStudent(struct Student* student)
 {
@@ -16,33 +9,3 @@ void printStudent(struct Student* student)
   printf("Student ID: %d\n", student -> studentID);
 }
 
-int main() {
-  int numStudent;
-  char strInput[256];
-  printf("How many students do you want to enter: \n");
-  fgets(strInput, 256, stdin);
-  sscanf(strInput, "%d", &numStudent);
-  struct Student student[numStudent];
-  for (int i = 0; i<numStudent; i++)
-  {
-    printf("You are entering student number: %d\n", i);
-    printf("First Name: ");
-    fgets(strInput, 256, stdin);
-    sscanf(strInput, "%s", student[i].firstName);
-    printf("Last Name: ");
-    fgets(strInput, 256, stdin);
-    sscanf(strInput, "%s", student[i].lastName);
-    printf("Age: ");
-    fgets(strInput, 256, stdin);
-    sscanf(strInput, "%d", &student[i].age);
-    printf("Student ID: ");
-    fgets(strInput, 256, stdin);
-    sscanf(strInput, "%d", &student[i].studentID);
-  }
-
-
-for (int i = 0; i<numStudent; i++)
-{
-  printStudent(&student[i]);
-}
-}
